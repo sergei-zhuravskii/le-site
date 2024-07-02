65e8484216dc729a9ba7aeecc65d3e8670175cc9
sergei-zhuravskii <117652478+sergei-zhuravskii@users.noreply.github.com>
2024-07-02 18:32:06 +0300

    Add files via upload

--- js/smooth-appearance-blocks.js
+++ js/smooth-appearance-blocks.js
@@ -1,4 +1,14 @@
 
+// assign class (smooth-block) to img and text ii section-about
+if (window.innerWidth < 768) {
+  const companyImg = document.querySelector('.list-about__company-img');
+  const companyText = document.querySelector('.list-about__company-text');
+
+  companyImg.classList.add('smooth-block');
+  companyText.classList.add('smooth-block');
+}
+
+// launch function for smooth appearance of blocks
 document.addEventListener('DOMContentLoaded', function() {
 
   let smoothBlocks = document.querySelectorAll('.smooth-block');
