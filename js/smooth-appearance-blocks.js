b797656aa81a876dba45354bb812ddbd8db3e81a
sergei-zhuravskii <117652478+sergei-zhuravskii@users.noreply.github.com>
2024-07-03 15:35:25 +0300

    Add files via upload

--- js/smooth-appearance-blocks.js
+++ js/smooth-appearance-blocks.js
@@ -1,8 +1,14 @@
 
 // assign class (smooth-block) to any blocks
+if (window.innerWidth < 992) {
+  const smoothBlockContainers = document.querySelectorAll('[data-name="smooth-block-container"]');
+
+  smoothBlockContainers.forEach (item => item.classList.add('smooth-block'));
+}
+
 if (window.innerWidth < 768) {
   const listAboutCompanyImg = document.querySelector('.list-about__company-img');
-  const listAboutCompanyText = document.querySelector('[data-name="smooth-block-container"]');
+  const listAboutCompanyText = document.querySelector('[data-name="smooth-block-container-about"]');
 
   listAboutCompanyImg.classList.add('smooth-block');
   listAboutCompanyText.classList.add('smooth-block');
@@ -33,19 +39,3 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 });
-
-// if (window.innerWidth < 768) {
-//   const listServicesItems = document.querySelectorAll('.list-services__item');
-//   const listServicesItemImgs = document.querySelectorAll('.list-services__item-img');
-//   const listServicesItemHeadings = document.querySelectorAll('.list-services__item-heading');
-//   const listServicesItemDescriptions = document.querySelectorAll('.list-services__item-description');
-//   const listAboutCompanyImg = document.querySelector('.list-about__company-img');
-//   const listAboutCompanyText = document.querySelector('.list-about__company-text');
-
-//   listServicesItems.forEach (item => item.classList.add('smooth-block'));
-//   listServicesItemImgs.forEach (img => img.classList.add('smooth-block'));
-//   listServicesItemHeadings.forEach (heading => heading.classList.add('smooth-block'));
-//   listServicesItemDescriptions.forEach (heading => heading.classList.add('smooth-block'));
-//   listAboutCompanyImg.classList.add('smooth-block');
-//   listAboutCompanyText.classList.add('smooth-block');
-// }
