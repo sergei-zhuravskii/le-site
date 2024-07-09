b0d8a6357bf4ebfc926a0cc43d68a8e5829ab1c7
Сергей Журавский <117652478+sergei-zhuravskii@users.noreply.github.com>
2024-07-09 13:09:11 +0300

    Update show-notice.js

--- js/show-notice.js
+++ js/show-notice.js
@@ -11,17 +11,22 @@ noticeLinks.forEach(link => {
 function openModal() {
 
   const modalContainer = document.querySelector('.notice__modal-container');
-  const modalBtn = document.querySelector('.modal__btn');
+  const noticeModalBtn = document.querySelector('#notice-modal-btn');
   const body = document.querySelector('body');
 
   modalContainer.classList.remove('hidden');
-  modalBtn.addEventListener('click', closeModal);
-  modalContainer.addEventListener('click', closeModal);
+  noticeModalBtn.addEventListener('click', closeModal);
   body.classList.add('body-scroll-off');
+  if (window.innerWidth > 770) {
+    body.classList.add('body-offset');
+  }
 
   function closeModal() {
     modalContainer.classList.add('hidden');
     body.classList.remove('body-scroll-off');
+    if (window.innerWidth > 770) {
+      body.classList.remove('body-offset');
+    }
   }
 
 }
