4a4b0363622b77867e4ca8f0bfcdc2f20ea4f8bf
Сергей Журавский <117652478+sergei-zhuravskii@users.noreply.github.com>
2024-07-10 15:53:05 +0300

    Update line-clamp-services.js

--- js/line-clamp-services.js
+++ js/line-clamp-services.js
@@ -4,14 +4,14 @@ const servicesButtonsMore = document.querySelectorAll('.list-services__btn-more'
 servicesButtonsMore.forEach((item) => {
   item.addEventListener('click', function() {
     const servicesDescriptionsOpen = document.querySelectorAll('.list-services__item-description:not(.list-services__item-description_line-clamp)');
-    const  servicesDescription = item.previousElementSibling;
+    const servicesDescription = item.previousElementSibling.firstElementChild;
 
     servicesDescription.classList.toggle('list-services__item-description_line-clamp');
     item.classList.toggle('btn-more_active');
 
     servicesDescriptionsOpen.forEach((item) => {
       item.classList.add('list-services__item-description_line-clamp');
-      item.nextElementSibling.classList.remove('btn-more_active');
+      item.parentElement.nextElementSibling.classList.remove('btn-more_active');
     })
   })
-})
+});
